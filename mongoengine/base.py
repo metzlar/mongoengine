51e26bb88a70f9504234c3280ef04fb46345cc04
Ivan Metzlar <metzlar@gmail.com>
2011-12-07 14:51:36 +0100

    Tiny patch to support pypy

--- mongoengine/base.py
+++ mongoengine/base.py
@@ -480,6 +480,8 @@ class TopLevelDocumentMetaclass(DocumentMetaclass):
                     base_meta['queryset_class'] = base._meta['queryset_class']
             try:
                 base_meta['objects'] = base.__getattribute__(base, 'objects')
+            except TypeError:
+                pass
             except AttributeError:
                 pass
 
